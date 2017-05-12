9b02a5cc146b44a596fd9c6a293a1ee221037248
Irina <myemail@gmail.com>
2017-05-12 11:20:03 -0700

    Changed class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstJavaProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstJavaProgramm.java
@@ -1,24 +1,24 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstJavaProgramm {
+//public class MyFirstJavaProgramm {
 
-  public static void main(String[] args) {
+  //public static void main(String[] args) {
 
-    Point p1 = new Point(10, 5);
+    //Point p1 = new Point(10, 5);
    // System.out.println(dot1(p1));
 
-    Point p2 = new Point(6,15);
+    //Point p2 = new Point(6,15);
    // System.out.println(dot2(p2));
 
-    System.out.println(distance(p1, p2));
-  }
+    //System.out.println(distance(p1, p2));
+ // }
 
 
-  public static double distance(Point p1, Point p2) {
+  //public static double distance(Point p1, Point p2) {
 
-    return Math.sqrt(((p1.y - p1.x) * (p1.y - p1.x))+((p2.y - p2.x) * (p2.y - p2.x)));
-  }
-}
+  //  return Math.sqrt(((p1.y - p1.x) * (p1.y - p1.x))+((p2.y - p2.x) * (p2.y - p2.x)));
+  //}
+//}
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -21,4 +21,22 @@ public Point(double x, double y) {
   public double dot2() {
     return (this.y - this.x) * (this.y - this.x);
   }
+
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(10, 5);
+    // System.out.println(dot1(p1));
+
+    Point p2 = new Point(6,15);
+    // System.out.println(dot2(p2));
+
+    System.out.println(distance(p1, p2));
+  }
+
+
+  public static double distance(Point p1, Point p2) {
+
+    return Math.sqrt(((p1.y - p1.x) * (p1.y - p1.x))+((p2.y - p2.x) * (p2.y - p2.x)));
+  }
 }
