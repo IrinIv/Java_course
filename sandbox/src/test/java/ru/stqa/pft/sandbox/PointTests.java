0a1e6924382a0f10674d24e50f7a0f7f6f7d4e9a
Irina <myemail@gmail.com>
2017-05-15 10:40:54 -0700

    Changed PointTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -13,7 +13,8 @@ public class PointTests {
 
     Point p1 = new Point(10, 5);
     Point p2 = new Point(6,15);
-    Assert.assertEquals(Math.sqrt((Math.pow((p1.p2 - p1.p1), 2)+(Math.pow((p2.p2 - p2.p1), 2)))), 10.295630140987);
+
+    Assert.assertEquals(p1.distanceTo(p2), 10.770329614269007);
 
   }
 
@@ -22,7 +23,7 @@ public class PointTests {
 
     Point p1 = new Point(2, 6);
     Point p2 = new Point(1,5);
-    Assert.assertEquals(Math.sqrt((Math.pow((p1.p2 - p1.p1), 2)+(Math.pow((p2.p2 - p2.p1), 2)))), 5.656854249492381);
+    Assert.assertEquals(p1.distanceTo(p2), 1.4142135623730951);
 
   }
 
@@ -31,7 +32,7 @@ public class PointTests {
 
     Point p1 = new Point(4, 8);
     Point p2 = new Point(6,8);
-    Assert.assertEquals(Math.sqrt((Math.pow((p1.p2 - p1.p1), 2)+(Math.pow((p2.p2 - p2.p1), 2)))), 4.47213595499958);
+    Assert.assertEquals(p1.distanceTo(p2), 2.0);
 
   }
 }
