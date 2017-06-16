6b1748584b5a3bf4a12a31787b1f99a09c5e31dc
Irina <myemail@gmail.com>
2017-06-16 11:56:50 -0700

    Added Provider xml for Contacts

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,18 +23,23 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object []> validContacts() throws IOException {
-    List<Object []> list = new ArrayList<Object []>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    //List<Object []> list = new ArrayList<Object []>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
     String line = reader.readLine();
     while(line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
-              .withAddress(split[2]).withHomephone(split[3])
-              .withEmail(split[4]).withGroup(split[5])});
+      //String[] split = line.split(";");
+      //list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
+      //        .withAddress(split[2]).withHomephone(split[3])
+      //        .withEmail(split[4]).withGroup(split[5])});
+      xml += line;
       line = reader.readLine();
     }
-
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    //return list.iterator();
   }
 
 
