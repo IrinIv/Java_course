bf7a47793c631aa01858a7bbb0199fcf42797235
Irina <myemail@gmail.com>
2017-06-16 12:36:46 -0700

    Implemented try() for Writers and Readers

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -60,9 +60,9 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
 
@@ -70,20 +70,21 @@ public class ContactDataGenerator {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try ( Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(format("%s;%s;%s;%s;%s;%s\n", contact.getFirstname(),
-              contact.getLastname(), contact.getAddress(),
-              contact.getHomephone(), contact.getEmail(), contact.getGroup()));
+    try ( Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(format("%s;%s;%s;%s;%s;%s\n", contact.getFirstname(),
+                contact.getLastname(), contact.getAddress(),
+                contact.getHomephone(), contact.getEmail(), contact.getGroup()));
+      }
+
     }
-    writer.close();
   }
 
   private List<ContactData> generateContacts(int count) {
