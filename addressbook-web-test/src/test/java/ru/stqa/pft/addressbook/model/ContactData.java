6dcc29ee3a27ca16fb74a5496209ec0aad5b787a
Irina <myemail@gmail.com>
2017-06-16 11:42:37 -0700

    Finished ContactDataGenerator xml&json format

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,21 +1,32 @@
 package ru.stqa.pft.addressbook.model;
 
-import java.io.File;
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+import java.io.File;
+ @XStreamAlias("contact")
 public class ContactData {
 
+   @XStreamOmitField
   private int id =Integer.MAX_VALUE; ;
+   @Expose
   private String firstname;
+   @Expose
   private String lastname;
+   @Expose
   private String address;
+   @Expose
   private String homephone;
   private String mobilephone;
   private String workphone;
   private String allphones;
+   @Expose
   private String email;
   private String email2;
   private String email3;
   private String allemails;
+   @Expose
   private String group;
   private File photo;
 
