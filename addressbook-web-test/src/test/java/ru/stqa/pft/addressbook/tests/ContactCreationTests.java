20aa6e88e43872eead1d0b4d944814cf6af9a547
Irina <myemail@gmail.com>
2017-06-16 12:37:25 -0700

    Changed ContactCreationTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,28 +25,47 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object []> validContacts() throws IOException {
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
     //List<Object []> list = new ArrayList<Object []>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while(line != null) {
-      //String[] split = line.split(";");
-      //list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
-      //        .withAddress(split[2]).withHomephone(split[3])
-      //        .withEmail(split[4]).withGroup(split[5])});
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        //String[] split = line.split(";");
+        //list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
+        //        .withAddress(split[2]).withHomephone(split[3])
+        //        .withEmail(split[4]).withGroup(split[5])});
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+      //return list.iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
-    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
-    //return list.iterator();
+    }
+
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+      }.getType());
+      return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+    }
   }
 
 
-  @Test(dataProvider = "validContacts")
+  @Test(dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) {
     app.contact().homePage();
     Contacts before = app.contact().all();
