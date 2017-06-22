5ce16a19c2f9dac1a294e519091be65869ad269d
Irina <myemail@gmail.com>
2017-06-22 16:52:32 -0700

    Added HbConnectionTest for Contacts

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,37 +3,64 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
-
+import org.hibernate.annotations.Type;
+import javax.persistence.*;
 import java.io.File;
- @XStreamAlias("contact")
+
+@XStreamAlias("contact")
+ @Entity
+ @Table(name = "addressbook")
 public class ContactData {
 
    @XStreamOmitField
+   @Id
+   @Column(name = "id")
   private int id =Integer.MAX_VALUE; ;
    @Expose
+   @Column(name = "firstname")
   private String firstname;
    @Expose
+   @Column(name = "lastname")
   private String lastname;
    @Expose
+   @Column(name = "address")
+   @Type(type = "text")
   private String address;
    @Expose
+   @Column(name = "home")
+   @Type(type = "text")
   private String homephone;
    @Expose
+   @Column(name = "mobile")
+   @Type(type = "text")
   private String mobilephone;
    @Expose
+   @Column(name = "work")
+   @Type(type = "text")
   private String workphone;
+   @Transient
   private String allphones;
    @Expose
+   @Column(name = "email")
+   @Type(type = "text")
   private String email;
    @Expose
+   @Column(name = "email2")
+   @Type(type = "text")
   private String email2;
    @Expose
+   @Column(name = "email3")
+   @Type(type = "text")
   private String email3;
+   @Transient
   private String allemails;
    @Expose
+   @Transient
   private String group;
    @Expose
-   private File photo;
+   @Column(name = "photo")
+   @Type(type = "text")
+   private String photo;
 
 
   public int getId() {
@@ -88,7 +115,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withId(int id) {
@@ -157,7 +184,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
