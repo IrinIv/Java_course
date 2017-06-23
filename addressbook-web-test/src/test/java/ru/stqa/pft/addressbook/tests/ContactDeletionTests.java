54d8fec5ba6433a445914488292a1422708430e6
Irina <myemail@gmail.com>
2017-06-22 22:19:18 -0700

    Changed ContactDeletionTests
    Passed

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -18,7 +18,7 @@ public class ContactDeletionTests extends TestBase {
   @BeforeMethod(enabled = true)
   public void ensurePreconditions() {
     app.contact().homePage();
-    if (app.contact().all().size() == 0) {
+    if (app.db().contacts().size() == 0) {
       app.goTo().contactPage();
       app.contact().create(new ContactData().withFirstname("I").withLastname("V").withAddress("ad").withHomephone("1234").withMobilephone("1234").withWorkphone("2345").withAllphones("").withEmail("email@gmail.com").withEmail2("email").withEmail3("email").withGroup("test1"), true);
     }
@@ -27,11 +27,11 @@ public class ContactDeletionTests extends TestBase {
   @Test(enabled = true)
   public void testContactDeletion() {
 
-    Contacts before = app.contact().all();
+    Contacts before = app.db().contacts();
     ContactData deletedContact = before.iterator().next();
     app.contact().delete(deletedContact);
     assertThat(app.contact().count(), equalTo(before.size() - 1));
-    Contacts after = app.contact().all();
+    Contacts after = app.db().contacts();
     assertThat(after, equalTo(before.withOut(deletedContact)));
   }
 }
