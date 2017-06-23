f4173e07585e8dc992ff388f93508d3fc174db55
Irina <myemail@gmail.com>
2017-06-22 18:36:13 -0700

    Changed GroupCreationTests  & GroupDeletionTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -14,19 +14,20 @@ public class GroupDeletionTests extends TestBase {
   @BeforeMethod(enabled = true)
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
-      app.group().create(new GroupData().withName("test2"));
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test3"));
     }
   }
 
   @Test(enabled = true)
   public void testGroupDeletion() {
 
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
     assertThat(app.group().count(), equalTo(before.size() - 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.withOut(deletedGroup)));
   }
 }
