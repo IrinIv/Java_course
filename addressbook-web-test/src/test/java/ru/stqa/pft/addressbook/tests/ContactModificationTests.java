9c2349fd06684d0ad76f9bc3d5e3c1c617002b16
Irina <myemail@gmail.com>
2017-06-23 12:06:43 -0700

    Created method verifyUI for Contacts
    Added for ContactModificationTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -39,7 +39,7 @@ public class ContactModificationTests extends TestBase {
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.db().contacts();
     assertThat(after, equalTo(before.withOut(modifiedContact).withAdded(contact)));
-
+    verifyContactListInUi();
   }
 
 
