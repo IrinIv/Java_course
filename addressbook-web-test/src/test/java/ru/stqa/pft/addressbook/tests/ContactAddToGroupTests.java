daeda0d1d502206afb2d668fe3d53c4f9a7f6145
Irina <myemail@gmail.com>
2017-06-27 21:01:23 -0700

    Changed again

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
@@ -1,25 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by IrinaIv on 6/23/2017.
@@ -46,33 +32,30 @@ public class ContactAddToGroupTests extends TestBase {
 
   @Test
   public void testContactAddToGroup() {
-  boolean selected = false;
+    boolean selected = false;
     app.contact().homePage();
     Groups allGroups = app.db().groups();
     Contacts before = app.db().contacts();
-    for(ContactData selectedContact : before)  {
+    for (ContactData selectedContact : before) {
       if (selected) break;
       Groups beforeSelectedGroups = selectedContact.getGroups();
-      for(GroupData group : allGroups) {
+      for (GroupData group : allGroups) {
         if (!beforeSelectedGroups.contains(group)) {
           app.contact().select(selectedContact, group);
           selected = true;
+          System.out.println(beforeSelectedGroups);
           break;
         }
       }
     }
     if (!selected) {
-      app.goTo().groupPage();
-      app.group().create(new GroupData().withName("test2"));
-      Groups addedGroups = app.db().groups();
-      GroupData group = addedGroups.stream()
+      Groups extraGroups = app.db().groups();
+      GroupData group = extraGroups.stream()
               .max((g1, g2) -> Integer.compare(g1.getId(), g2.getId())).get();
       ContactData contact = app.db().contacts().iterator().next();
-      app.contact().select(contact, group);
-    }
-
     }
   }
+}
 
 
 
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteFromGroupTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteFromGroupTests.java
@@ -1,25 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by IrinaIv on 6/23/2017.
@@ -43,6 +29,7 @@ public class ContactDeleteFromGroupTests extends TestBase {
               .withEmail2("email").withEmail3("email").inGroup(groups.iterator().next()), true);
     }
   }
+
   @Test
   public void testContactDeleteFromGroup() {
 
@@ -50,18 +37,27 @@ public class ContactDeleteFromGroupTests extends TestBase {
     app.contact().homePage();
     Groups allGroups = app.db().groups();
     Contacts before = app.db().contacts();
-    for(ContactData deletedContact : before)  {
+    for (ContactData deletedContact : before) {
       if (deleted) break;
       Groups beforeDeletedGroups = deletedContact.getGroups();
-      for(GroupData group : allGroups) {
+      for (GroupData group : allGroups) {
         if (beforeDeletedGroups.contains(group)) {
           app.contact().deleteContactFromGroup(deletedContact, group);
           deleted = true;
           break;
         }
       }
+    }
+
+    if (!deleted) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test2"));
+      Groups deletedGroups = app.db().groups();
+      GroupData group = deletedGroups.stream()
+              .max((g1, g2) -> Integer.compare(g1.getId(), g2.getId())).get();
+      ContactData contact = app.db().contacts().iterator().next();
 
     }
   }
-  }
+}
 
