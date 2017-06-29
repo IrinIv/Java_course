cd6af4c46af4f647239cffad6b8d7a6be111ecfa
Irina <myemail@gmail.com>
2017-06-29 16:43:14 -0700

    Added FtpHelper

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -11,8 +11,6 @@ public class RegistrationTests extends TestBase{
   public void testRegistration() {
     app.registration().start("user1", "user1@gmail.com");
 
-
-
   }
 
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by IrinaIv on 5/18/2017.
  */
@@ -17,10 +20,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bac");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.bac","config_inc.php" );
     app.stop();
   }
 
