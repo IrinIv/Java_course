ed7f34e2a0fdc78cdc56497d55a78c9d82c8bef0
IrinIv <iryna.ivanova2@gmail.com>
2017-07-09 20:40:05 -0700

    Update GithubTests.java

--- github_sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github_sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -14,8 +14,8 @@ public class GithubTests {
   @Test
   public void testCommits() throws IOException {
 
-    Github github = new RtGithub("a947a08420a7dd80e5f74a6d8371a4514b72304c");
-    RepoCommits commits = github.repos().get(new Coordinates.Simple("IrinIv", "Java_course")).commits();
+    Github github = new RtGithub("***************************************");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("******", "******")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableBiMap.Builder<String, String>().build())){
       System.out.println(new RepoCommit.Smart(commit).message());
 
